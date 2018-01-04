a332fc8e493a4bc3fd775d46096b485b8ca4c20d
ewarrenG <eglasenk@gmail.com>
2018-01-04 11:28:56 -0500

    Added files and changes required for deployment to heroku

--- moovies/settings.py
+++ moovies/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'v=$+3lrmi!u9*+z^d4)hz#6c$lir#v
 #DEBUG = True
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
